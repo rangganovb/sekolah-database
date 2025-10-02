93f9c095050385563e08298aa2a64181b356dabe
rangganovb <ranggamum@gmail.com>
2025-10-02 23:20:50 +0700

    Menghapus File tidak terpakai ketiga

--- select_rata_siswa.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO "select siswa.nama, AVG(nilai.nilai) as rata_nilai
-from siswa
-join nilai on siswa.id = nilai.siswa_id
-group by siswa.nama" (nama,rata_nilai) VALUES
-	 ('Andi',87.5000000000000000),
-	 ('Putra',81.5000000000000000),
-	 ('Rika',88.0000000000000000),
-	 ('Budi',85.0000000000000000),
-	 ('Nadia',87.5000000000000000);
